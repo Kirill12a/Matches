60905c12a6a159efc2ad0ef1e344e7e569f83b6f
Kirill12a <kirilldrozd12@gmail.com>
2021-12-26 23:45:59 +0300

    маленький редакт

--- Matches/MainView/ViewController.swift
+++ Matches/MainView/ViewController.swift
@@ -12,9 +12,9 @@ class ViewController: UIViewController {
 
     private let cellIdentifire = "cellID"
     
-    var ar1 = ["Спартак","Зенит"]
-    var arr2 = ["Барселона", "Реал"]
-    var arr3 = [">", "="]
+    var ar1 = ["Спартак","Зенит","ЦСК"]
+    var arr2 = ["Барселона", "Реал", "Порту"]
+    var arr3 = [">", "=", "<"]
     
     
     override func viewDidLoad() {
