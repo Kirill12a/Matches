91a3044be19c13aa3d44e65e566fab933276ac6a
Kirill12a <kirilldrozd12@gmail.com>
2021-12-27 22:32:11 +0300

    ......

--- Matches/MainView/CellTableView&Xib/CellWithMatches.swift
+++ Matches/MainView/CellTableView&Xib/CellWithMatches.swift
@@ -9,7 +9,6 @@ import Foundation
 import UIKit
 
 class CellWithMatches: UITableViewCell {
-    
     @IBOutlet weak var TeamOne: UILabel!
     @IBOutlet weak var TeamTwo: UILabel!
     @IBOutlet weak var ResultMatches: UILabel!
