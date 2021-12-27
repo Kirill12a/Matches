1a907efec3356db005c2e8eea12a5d6b6bbd74bc
Kirill12a <kirilldrozd12@gmail.com>
2021-12-27 21:35:33 +0300

    1/2 второго экрана готова

--- Matches/CreateMatches/CreateViewController.swift
+++ Matches/CreateMatches/CreateViewController.swift
@@ -6,24 +6,93 @@
 //
 
 import UIKit
+import SnapKit
 
 class CreateViewController: UIViewController {
-
+    private let items = [">", "=", "<"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        initilize()
+        
     }
     
+    
+    private func initilize(){
+        
+        let textFieldFirstTeam: UITextField = {
+            let tf = UITextField()
+            tf.placeholder = "Введите плиз"
+            tf.borderStyle = .line
+            
+            tf.clipsToBounds = true
+            
+           return tf
+        }()
+        
+        view.addSubview(textFieldFirstTeam)
+        textFieldFirstTeam.snp.makeConstraints { make in
+            make.top.equalTo(view.bounds.minY + 100)
+            make.left.equalToSuperview().inset(5)
+            make.width.equalTo(view.bounds.width / 2 - 7) // дела првоерку на то как прыгает тф
 
-    /*
-    // MARK: - Navigation
+            
+        }
+        
+        
+        let textFieldSecondTeam: UITextField = {
+            let tf = UITextField()
+            tf.placeholder = "Введите плиз"
+            tf.borderStyle = .line
+            
+            tf.clipsToBounds = true
+            
+           return tf
+        }()
+        
+        view.addSubview(textFieldSecondTeam)
+        textFieldSecondTeam.snp.makeConstraints { make in
+            make.top.equalTo(view.bounds.minY + 100)
+            make.right.equalToSuperview().inset(5)
+//            make.left.equalTo(textFieldFirstTeam).offset(5)
+            make.width.equalTo(view.bounds.width / 2 - 7) // дела првоерку на то как прыгает тф
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            
+            
+            
+            let winSegmented: UISegmentedControl = {
+               var segmneted = UISegmentedControl(items: items)
+                segmneted.selectedSegmentIndex = 0
+                segmneted.tintColor = UIColor.black
+                segmneted.addTarget(self, action: #selector(self.filterApply), for: .valueChanged)
+                return segmneted
+            }()
+            
+            self.view.addSubview(winSegmented)
+            winSegmented.snp.makeConstraints { make in
+                make.right.left.equalToSuperview().inset(50)
+                make.height.equalTo(50)
+                make.width.equalTo(view.bounds.width - 25)
+                make.centerX.equalToSuperview()
+                make.top.equalTo(textFieldFirstTeam.snp_bottomMargin).offset(20)
+            }
+            
+            
+        }
+        
+        
     }
-    */
+    
+    
+    @objc private func filterApply(segment: UISegmentedControl) -> Void {
+        switch segment.selectedSegmentIndex {
+        case 1: break
+        case 2: break
+        case 3: break
+        default: break
+            
+        }
+    }
 
 }
