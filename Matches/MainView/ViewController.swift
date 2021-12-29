3c230ab6f0d27196cf3a13947298523a7fe2732c
Kirill12a <kirilldrozd12@gmail.com>
2021-12-29 21:06:09 +0300

    Добавил CoreData(она работает)

--- Matches/MainView/ViewController.swift
+++ Matches/MainView/ViewController.swift
@@ -7,14 +7,35 @@
 
 import UIKit
 import SnapKit
+import CoreData
+
+var noteList = [Note]()
+
 
 class ViewController: UIViewController {
+    var firstLoad = true
 
     private let cellIdentifire = "cellID"
     
     var ar1 = ["Спартак","Зенит","ЦСК", "УФА"]
     var arr2 = ["Барселона", "Реал", "Порту", "Химки"]
     var arr3 = [">", "=", "<", "="]
+    
+  
+    
+    
+    
+    func nonDeletedNotes() ->[Note]{
+        var noDeletedNoteList = [Note]()
+        for note in noteList{
+            if note.deletedDate == nil{
+                noDeletedNoteList.append(note)
+            }
+        }
+        return noDeletedNoteList
+    }
+    
+    
     
     
     override func viewDidLoad() {
@@ -26,6 +47,22 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         navigationItem.title = "Matches"
         let barButton = navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(transtionVC))
+        
+        if firstLoad {
+            firstLoad = false
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Note")
+            do {
+                let results: NSArray = try context.fetch(request) as NSArray
+                for result in results {
+                    let note = result as! Note
+                    noteList.append(note)
+                }
+            } catch {
+                print("Fetch Failed")
+            }
+        }
     }
     
     @objc func transtionVC(){
@@ -72,17 +109,34 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        ar1.count
+//        ar1.count
+        nonDeletedNotes().count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifire) as? CellWithMatches
-        cell?.TeamOne.text = ar1[indexPath.row]
-        cell?.TeamTwo.text = arr2[indexPath.row]
-        cell?.ResultMatches.text = arr3[indexPath.row]
+//        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifire) as? CellWithMatches
+//        cell?.TeamOne.text = ar1[indexPath.row]
+//        cell?.TeamTwo.text = arr2[indexPath.row]
+//        cell?.ResultMatches.text = arr3[indexPath.row]
+//
+//        return cell!
         
-        return cell!
+        let noteCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifire, for: indexPath) as! CellWithMatches
+        let thisNote: Note!
+        thisNote = nonDeletedNotes()[indexPath.row]
+        noteCell.TeamOne.text = thisNote.teamOne
+        noteCell.TeamTwo.text = thisNote.teamTwo
+        noteCell.ResultMatches.text = thisNote.teamWin
+        
+        return noteCell
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    
+   
     
     
 }
